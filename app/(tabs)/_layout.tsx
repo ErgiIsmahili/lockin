690a8f418f6787c6f575ac66d73b786cff39381a
ErgiIsmahili <ergi.ismahili@gmail.com>
2024-09-10 23:52:29 -0400

    r - changed explore page name to groups

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -24,7 +24,7 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="groups"
         options={{
           title: 'Groups',
           tabBarIcon: ({ color, focused }) => (
