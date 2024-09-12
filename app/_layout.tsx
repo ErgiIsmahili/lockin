1ffaf0fc7bf3495bcda70cdc2d1655a39da301a2
ErgiIsmahili <ergi.ismahili@gmail.com>
2024-09-11 23:25:42 -0400

    f - hid screen headers

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout() {
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
         <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-          <Stack>
+          <Stack screenOptions={{ headerShown: false }}>
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
             <Stack.Screen name="+not-found" />
           </Stack>
